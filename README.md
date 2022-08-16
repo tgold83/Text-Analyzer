5f2cb942efeb59d2974023d913681999ae905368
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-16 14:59:49 -0700

    BUGFIX JS to count no imput as zero value
    Co-authored-by: Tim Empey <timothyempey@gmail.com>

--- README.md
+++ README.md
@@ -93,6 +93,13 @@ const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
 
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
+
 Describe: ommitOffensiveText()
 
 Test: "It should return 0 occurences for an empty string"
--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
-  }  
+  }
   let wordCount = 0;
   const textArray = text.split(" ");
-  textArray.forEach(function(word) {
+  textArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
     }
@@ -15,6 +15,9 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
@@ -24,6 +27,7 @@ function numberOfOccurrencesInText(word, text) {
   });
   return wordCount;
 }
+
 
 
 function ommitOffensiveText(word, text) {
@@ -36,3 +40,19 @@ function ommitOffensiveText(word, text) {
   });
   return textArray;
 }
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
