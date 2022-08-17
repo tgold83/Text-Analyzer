5e4edf83db144bf4a8d762838a94561aa51fcf29
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-17 14:12:55 -0700

    add for statement to utility logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 // Utility Logic
 
-function isEmpty(testString) {
-  return (testString.trim().length === 0);
+function isEmpty() {
+  for (let i=0; i < arguments.length; i++) {
+    if (arguments[i].trim().length === 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // Business Logic
@@ -72,7 +77,7 @@ window.addEventListener("load", function() {
 });
 
 function boldPassage(word, text) {
-  if (isEmpty(word) || isEmpty(text)) {
+  if (isEmpty(word, text)) {
     return null;
   }
   p = document.createElement("p");
